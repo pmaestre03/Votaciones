0bf8df9180710ba8b498b1b442e3061fbf5ed94f
«litsu987» <navioaleix@gmail.com>
2024-01-18 20:21:31 +0100

    primercommit

---
+++ footer.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>header.php</title>
+    <link rel="stylesheet" href="Utilidades/styles.css">
+</head>
+<body>
+    <footer>
+        <div class="footer-content">
+            <div class="contact-list">
+                <p class="contact">Pau +34 123 456 789</p>
+                <p class="contact">Xavi +34 987 654 321</p>
+                <p class="contact">Aleix +34 555 123 456</p>
+            </div>
+            <div class="footer-links">
+                <p><a href="http://www.iesesteveterradas.cat" target="_blank"><u>www.iesesteveterradas.cat</u></a></p>
+                <p><a href="https://github.com/pmaestre03/Votaciones/tree/produccion" target="_blank"><u> Nuestro GitHub </u></a></p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
