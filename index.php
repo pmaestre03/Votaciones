7ef0ec51fead5b1d7ef90e3a4028b9e091abddae
pmaestre03 <maestrep15@gmail.com>
2024-01-18 20:37:55 +0100

    a

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Document</title>
+          <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+          <div>
+                    <img src="./images/votaciones.jpeg" alt="">
+                    <p>Nuestro programa esta basado en un sistema de votaciones, donde podemos iniciar sesion y registrarnos, una vez hemos iniciado sesion se nos permite crear nuevas encuestas en nuestro panel de control</p>
+          </div>
+</body>
+</html>
