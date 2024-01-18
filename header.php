4c167391c2b706fc2f671533f32b4d303b06c901
Xavi Martinez <77339181+xaavii0711@users.noreply.github.com>
2024-01-18 20:14:38 +0100

    Add header & footer

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>header.php</title>
+    <link rel="stylesheet" href="Utilidades/styles.css">
+</head>
+<body>
+    <header>
+        <button class="button">SIGNUP</button>
+        <h1>Tu Sitio Web</h1>
+        <button class="button button-login">LOGIN</button>
+    </header>
+</body>
+</html>
