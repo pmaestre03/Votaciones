b6d8a52ebdd8e269adea0bd4d2164484247117bc
pmaestre03 <maestrep15@gmail.com>
2024-01-19 16:12:12 +0100

    header y footer

--- header.php
+++ header.php
@@ -3,11 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tu Sitio Web</title>
+    <title>header.php</title>
     <link rel="stylesheet" href="Utilidades/styles.css">
 </head>
 <body>
-    <?php include("header.php") ?>
-    <?php include("footer.php") ?>
+    <header>
+        <button class="button">SIGNUP</button>
+        <h1>Tu Sitio Web</h1>
+        <button class="button button-login">LOGIN</button>
+    </header>
 </body>
 </html>
