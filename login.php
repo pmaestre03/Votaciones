5766e147dea8c9d4c73b3fee49fc6a3a13c6e880
Pau Maestre <96410501+pmaestre03@users.noreply.github.com>
2024-02-01 10:23:08 +0100

    prueba

--- login.php
+++ login.php
@@ -63,20 +63,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION['id_user'] = $row["id_user"];
             $condiciones_aceptadas = $row['condiciones_aceptadas'];
             $_SESSION['condiciones_aceptadas'] = $condiciones_aceptadas;
+            if ($row['token_validado'] == 0) {
+                echo "<script>showNotification('Token no validado','red')</script>";
+            }
             if ($condiciones_aceptadas == 0 && $row['token_validado'] == 1) {
                 header("Location: aceptar_condiciones.php");
                 exit();
             } else {
-                if ($row['token_validado'] === 0) {
-                        echo "<script>showNotification('Token no validado','red')</script>";
-                } else {
                         $_SESSION['email'] = $row["email"];
                         $_SESSION['usuario'] = $row['nombre'];;
                         echo "Usuario Correcto: Hola $nombre_usuario";
                         registrarEvento("Inicio de sesión por el usuario: $usuario");
                         header("Location: dashboard.php");
                         exit();
-                }
             }     
         } else {
                             $usuarioIntentado = htmlspecialchars($_POST["username"]);
